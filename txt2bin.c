57c3cc747a9d9fc51c1754a544b61afc8e6a6884
qianlihu8906 <wangzhiqiang8906@gmai.com>
2018-06-12 22:13:44 +0800

    code complete

--- txt2bin.c
+++ txt2bin.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <assert.h>
-
+#include <endian.h>
 #include <ctype.h>
-/*
-	input   txt: bigendian  little 
-	output  bin: bigendian  little
-*/
+#include <unistd.h>
 
-
-#define BUFFER_LEN	2048
 struct context{
 	FILE *in_fp;
+#define BUFFER_LEN	2048
 	char in_buffer[BUFFER_LEN];
 	int in_buffer_pos;
 	int in_buffer_len;
-
+#define ENDIAN_HOST		 0
+#define ENDIAN_LITTLE		 1
+#define ENDIAN_BIG   		 2
+	char endian;
 	FILE *out_fp;
 };
 
 static int load_in_buffer(struct context *c);
-static int init(struct context *c,const char *in_path,const char *out_path)
+static int init(struct context *c,const char *in_path,const char *out_path,char endian)
 {
 	memset(c,0,sizeof(*c));
+	c->endian = endian;
 	FILE *in_fp =  fopen(in_path,"r");
 	if(in_fp == NULL)
 		return -1;
@@ -43,6 +44,7 @@ static int init(struct context *c,const char *in_path,const char *out_path)
 static void finish(struct context *c)
 {
 	fclose(c->in_fp);
+	fclose(c->out_fp);
 }
 
 static int load_in_buffer(struct context *c)
@@ -99,6 +101,25 @@ static void parse_whitespace(struct context *c)
 }
 
 
+static int compile(struct context *c,const char *buf)
+{
+	uint32_t number = strtol(buf,NULL,0);
+	switch(c->endian){
+		case ENDIAN_HOST:
+			break;
+		case ENDIAN_LITTLE:
+			number = htole32(number);
+			break;
+		case ENDIAN_BIG:
+			number = htobe32(number);
+			break;
+		default:
+			break;
+	};
+
+	fwrite(&number,sizeof(number),1,c->out_fp);
+	return 0;
+}
 
 static int parse_number(struct context *c)
 {
@@ -110,32 +131,69 @@ static int parse_number(struct context *c)
 			next_token(c);
 			break;
 		}
-		if(t == EOF)
+		if(t != 'x' && !isxdigit(t)){
+			printf("unexpected character %c\n",t);
 			return -1;
+		}
 		next_token(c);
 		buf[i++] = t;
 		assert(i < 64);
 	}
-	fprintf(c->out_fp,"0x%8.8x\n",(unsigned)strtol(buf,NULL,0));
-	return 0;
+	return compile(c,buf);
 }
 
 static int parse(struct context *c)
 {
 	parse_whitespace(c);
 	while(1){
-		int r = parse_number(c);	
+		int r = get_token(c);
+		if(r == EOF)
+			return 0;
+		r = parse_number(c);	
 		if(r < 0)
-			break;
+			return -1;
 		parse_whitespace(c);
 	}
-	return 0;
 }
 
-int main()
+static void print_usage(const char *name)
 {
+	printf("Usage: %s -i infile -o outfile -[hlb]\n",name);
+	printf("       -h host_endian\n");
+	printf("       -l little_endian\n");
+	printf("       -b big_endian\n");
+	exit(0);
+}
+
+int main(int argc,char **argv)
+{
+	const char *infile = NULL;
+	const char *outfile = NULL;
 	struct context c;
-	init(&c,"dat.txt","dat.bin");
+	char endian = ENDIAN_HOST;
+	int opt;
+	while((opt=getopt(argc,argv,"i:o:hlb")) != -1){
+		switch(opt){
+			case 'i':
+				infile = optarg;
+				break;
+			case 'o':
+				outfile = optarg;
+				break;
+			case 'h':
+				endian = ENDIAN_HOST;
+				break;
+			case 'l':
+				endian = ENDIAN_LITTLE;
+				break;
+			case 'b':
+				endian = ENDIAN_BIG;
+				break;
+		};
+	}
+	if(infile == NULL || outfile == NULL)
+		print_usage(argv[0]);
+	init(&c,infile,outfile,endian);
 	parse(&c);
 	finish(&c);
 }
